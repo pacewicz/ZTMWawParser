1dcd6f2c8f1bb892e1f3174239dba10fa637263b
Patryk Acewicz <pacewiczhepfner@sigma.ug.edu.pl>
2011-12-14 02:57:48 +0100

    podst. obsluga bledow

--- GetPrzystanek_final.sh
+++ GetPrzystanek_final.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-#$1 przystanek (wielkimi literami) | stop (upper-case)
+#$1 przystanek | stop
 #$2 czas w formacie hh:mm | time
 #$3 data w formacie dd.mm.yyyy | date
 #$4 maksymalna ilosc zwracanych wynikow (w formacie n) | max. # of returned results
 
-STEPONE=$(wget -O- -q "http://wyszukiwarka.ztm.waw.pl/bin/stboard.exe/pnx?input=$1&boardType=dep&time=$2%2B1&productsFilter=1111110111\&date=$3&maxJourneys=$4&start=yes" | head -n-15 )
+if [ -z "$4" ]
+then
+  echo "Usage: `basename $0` stop_name time date max_res"
+  echo "   stop_name - the name of the stop"
+  echo "   time      - in hh:mm format"
+  echo "   date      - in dd.mm.yyyy format"
+  echo "   max_res   - the maximum number of results returned"
+  exit 1
+fi
+
+sn=$(echo "$1" | tr 'a-z' 'A-Z')
+STEPONE=$(wget -O- -q "http://wyszukiwarka.ztm.waw.pl/bin/stboard.exe/pnx?input=$sn&boardType=dep&time=$2%2B1&productsFilter=1111110111\&date=$3&maxJourneys=$4&start=yes" | head -n-15 )
 #echo "$STEPONE" > STEPONE.txt
-STEPONEHALF=$(echo "$STEPONE" | wget --ignore-tags=link,img -i- -q --force-html -r -O- -np | sed -e ':a;N;$!ba;s/ <\/td>\n/ /g' -e 's/<[^>]*>//g' -e 's/&lt;/</g' -e 's/&gt;/>/g' -e 's/&amp;/\&/g' -e 's/&#322;/l/g' -e 's/ \{1,\}/ /g' -e 's/&#379;/Z/g' -e 's/&#321;/L/g' -e 's/&#323;/N/g' -e 's/&#280;/E/g' -e 's/&#211;/O/g' -e 's/&#260;/A/g' -e 's/&#346;/S/g' | egrep -a -e "N*[0-9]{1,} do [A-Z0-9 +.-]*" -e "$1")
+STEPONEHALF=$(echo "$STEPONE" | wget --ignore-tags=link,img -i- -q --force-html -r -O- -np | sed -e ':a;N;$!ba;s/ <\/td>\n/ /g' -e 's/<[^>]*>//g' -e 's/&lt;/</g' -e 's/&gt;/>/g' -e 's/&amp;/\&/g' -e 's/&#322;/l/g' -e 's/ \{1,\}/ /g' -e 's/&#379;/Z/g' -e 's/&#321;/L/g' -e 's/&#323;/N/g' -e 's/&#280;/E/g' -e 's/&#211;/O/g' -e 's/&#260;/A/g' -e 's/&#346;/S/g' | egrep -a -e "N*[0-9]{1,} do [A-Z0-9 +.-]*" -e "$sn")
 #echo "$STEPONEHALF" > STEPONEHALF.txt
 STEPTWO=$(echo "$STEPONE" | sed -e 's/<[^>]*>//g' -e 's/ $//g' -e 's/&lt;/</g' -e 's/&gt;/>/g' -e 's/&amp;/&/g' -e 's/&#322;/l/g' -e 's/ \{1,\}/ /g' -e 's/&#379;/Z/g' -e 's/&#321;/L/g' -e 's/&#323;/N/g' -e 's/&#280;/E/g' -e 's/&#211;/O/g' -e 's/&#260;/A/g' -e 's/&#346;/S/g' -e 's/>>/do/g')
 #echo "$STEPTWO" > STEPTWO.txt

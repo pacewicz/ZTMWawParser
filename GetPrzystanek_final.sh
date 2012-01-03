c93403c597bb751dcf93eb8704f02975bf8bb4de
Patryk Acewicz <pacewiczhepfner@sigma.ug.edu.pl>
2012-01-04 00:03:31 +0100

    input case bug fix, general improvements

--- GetPrzystanek_final.sh
+++ GetPrzystanek_final.sh
@@ -8,23 +8,38 @@
 if [ -z "$4" ]
 then
   echo "Usage: `basename $0` stop_name time date max_res"
-  echo "   stop_name - the name of the stop"
-  echo "   time      - in hh:mm format"
+  echo "   stop_name - the name of the stop; subst. spaces with \",\""
+  echo "   time      - in hh:mm format or \"now\""
   echo "   date      - in dd.mm.yyyy format"
   echo "   max_res   - the maximum number of results returned"
   exit 1
 fi
 
-sn=$(echo "$1" | tr 'a-z' 'A-Z')
-STEPONE=$(wget -O- -q "http://wyszukiwarka.ztm.waw.pl/bin/stboard.exe/pnx?input=$sn&boardType=dep&time=$2%2B1&productsFilter=1111110111\&date=$3&maxJourneys=$4&start=yes" | head -n-15 )
+sn=$(echo "$1" | sed -e 's/+/%2B/g' | tr 'a-z, ' 'A-Z++')
+#wielkimi literami, escape plusa, przecinki i spacje na plusy (=spacje)
+sns=$(echo "$1" | tr 'a-z,' 'A-Z ')
+#dla grepa - wyszukiwanie przystanku na liscie odjazdow
+if [[ $2 = now ]]
+then
+  tm=$(date +%H:%M)
+else
+  tm=$2
+fi
+STEPONE=$(wget -O- -q "http://wyszukiwarka.ztm.waw.pl/bin/stboard.exe/pnx?input=$sn&boardType=dep&time=$tm%2B1&productsFilter=1111110111\&date=$3&maxJourneys=$4&start=yes" | head -n-15 )
 #echo "$STEPONE" > STEPONE.txt
-STEPONEHALF=$(echo "$STEPONE" | wget --ignore-tags=link,img -i- -q --force-html -r -O- -np | sed -e ':a;N;$!ba;s/ <\/td>\n/ /g' -e 's/<[^>]*>//g' -e 's/&lt;/</g' -e 's/&gt;/>/g' -e 's/&amp;/\&/g' -e 's/&#322;/l/g' -e 's/ \{1,\}/ /g' -e 's/&#379;/Z/g' -e 's/&#321;/L/g' -e 's/&#323;/N/g' -e 's/&#280;/E/g' -e 's/&#211;/O/g' -e 's/&#260;/A/g' -e 's/&#346;/S/g' | egrep -a -e "N*[0-9]{1,} do [A-Z0-9 +.-]*" -e "$sn")
+STEPONEHALF=$(echo "$STEPONE" | wget --ignore-tags=link,img -i- -q --force-html -r -O- -np | sed -e ':a;N;$!ba;s/ <\/td>\n/ /g' -e 's/<[^>]*>//g' -e 's/&lt;/</g' -e 's/&gt;/>/g' -e 's/&amp;/\&/g' -e 's/&#322;/l/g' -e 's/ \{1,\}/ /g' -e 's/&#379;/Z/g' -e 's/&#321;/L/g' -e 's/&#323;/N/g' -e 's/&#280;/E/g' -e 's/&#211;/O/g' -e 's/&#260;/A/g' -e 's/&#346;/S/g' | egrep -a -e "N*[0-9]{1,} do [A-Z0-9 +.-]*" -e "$sns")
 #echo "$STEPONEHALF" > STEPONEHALF.txt
-STEPTWO=$(echo "$STEPONE" | sed -e 's/<[^>]*>//g' -e 's/ $//g' -e 's/&lt;/</g' -e 's/&gt;/>/g' -e 's/&amp;/&/g' -e 's/&#322;/l/g' -e 's/ \{1,\}/ /g' -e 's/&#379;/Z/g' -e 's/&#321;/L/g' -e 's/&#323;/N/g' -e 's/&#280;/E/g' -e 's/&#211;/O/g' -e 's/&#260;/A/g' -e 's/&#346;/S/g' -e 's/>>/do/g')
+# old STEPTWO=$(echo "$STEPONE" | sed -e 's/<[^>]*>//g' -e 's/ $//g' -e 's/&lt;/</g' -e 's/&gt;/>/g' -e 's/&amp;/&/g' -e 's/&#322;/l/g' -e 's/ \{1,\}/ /g' -e 's/&#379;/Z/g' -e 's/&#321;/L/g' -e 's/&#323;/N/g' -e 's/&#280;/E/g' -e 's/&#211;/O/g' -e 's/&#260;/A/g' -e 's/&#346;/S/g' -e 's/>>/do/g')
+STEPTWO=$(echo "$STEPONE" | sed -e ':a;N;$!ba;s/ <\/td>\n/ /g' -e 's/<[^>]*>//g' -e 's/&lt;/</g' -e 's/&gt;/>/g' -e 's/&amp;/\&/g' -e 's/&#322;/l/g' -e 's/ \{1,\}/ /g' -e 's/&#379;/Z/g' -e 's/&#321;/L/g' -e 's/&#323;/N/g' -e 's/&#280;/E/g' -e 's/&#211;/O/g' -e 's/&#260;/A/g' -e 's/&#346;/S/g' -e 's/>>/do/g')
 #echo "$STEPTWO" > STEPTWO.txt
-#echo "$(echo "$STEPTWO" | uniq | tac | head -n-7 | tac | grep -v "^$" | sed -r -e 's/[0-2][0-3][:][0-5][0-9]/&=/g')" > 3int.txt
+#echo "$(echo "$STEPTWO" | uniq | tac | head -n-7 | tac | grep -v '^$' | sed -r -e 's/[0-2][0-3]:[0-5][0-9]/\(\&\)=/g')" > 3int.txt
 STEPTHREE=$(echo "$STEPTWO" | uniq | tac | head -n-7 | tac | grep -v '^$' | sed -r -e 's/[0-2][0-3]:[0-5][0-9]/\(\&\)=/g' | tr "\n" " " | egrep -r -o -e 'N?[0-9]{1,3}[^:]*:[0-5][0-9]')
 #echo "$STEPTHREE" > STEPTHREE.txt
 #res=$(echo "$( echo "$STEPONEHALF" ; echo '##FEED##'; echo "$STEPTHREE")" | ./ListRep.exe )
-res=$(echo "$( echo "$STEPONEHALF" ; echo '##FEED##'; echo "$STEPTHREE")")
+if [ -z "$STEPONEHALF" ]
+then
+  res="<NO ENTRY>"
+else
+  res=$(echo "$(echo "$STEPONEHALF" ; echo '##FEED##'; echo "$STEPTHREE")")
+fi
 echo "$res"
